3aa2f90bff2fb026187558e69ec538ca3984cdad
Isaiah Kantor <isaiahkantor23@gmail.com>
2024-08-13 12:06:37 -0400

    Done today

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
-
+import json
 from models import db, Earthquake
 
 app = Flask(__name__)
@@ -20,7 +20,39 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def get_by_id(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return jsonify({
+            'id': earthquake.id,
+            'magnitude': earthquake.magnitude,
+            'location': earthquake.location,
+            'year': earthquake.year
+        }), 200
+    else:
+        return jsonify({'message': 'Earthquake 9999 not found.'}), 404
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_mag(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    quake_list = [
+        {
+            'id': quake.id,
+            'magnitude': quake.magnitude,
+            'location': quake.location,
+            'year': quake.year
+        }
+        for quake in earthquakes
+    ]
+    
+    response = {
+        'count': len(quake_list),
+        'quakes': quake_list
+    }
+
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
